27ab92005fdfe9c9bd89bddffe8c6cf6700c0d84
Nasim <nasimahmed0113@gmail.com>
2022-07-13 10:05:41 +0100

    DOM testing set up

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+document.querySelector(".title").textContent = "Hellooooo"
+
+const div = document.querySelectorAll("div")
+
+div.forEach ((d, i) => {
+    d.textContent = `LOOOoooooooooooooooooL at ${i}`
+})
